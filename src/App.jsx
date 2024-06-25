e2963cc3c34b309e0397db83767a993be9bf065d
smartdev00 <gladwynanderson95@gmail.com>
2024-06-25 10:38:15 -0700

    Modify App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
+
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import Test from "./pages/Test";
+import Home from "./pages/Home";
+import Menu from './pages/Menu'
+import Profile from './pages/Profile'
+import Offers from './pages/Offers'
+import More from './pages/More'
+
 
 const App = () => {
   return (
-    <div className="bg-slate-900 h-full min-h-screen">
+    // <div className="bg-sky-900 h-full min-h-screen">
+    <div className="container bg-sky-400 min-h-screen">
       <BrowserRouter>
-        {/* <AuthProvider> */}
+
           <Routes>
-            {/* <Route path="/auth" element={<Authentication />} />
+     
             <Route path="/" element={<Home />} />
-            <Route path="/create-post" element={<CreatePost />} />
-            <Route path="/update/:id" element={<UpdatePost />} /> */}
+
+            <Route path="/Menu" element={<Menu />} />
+            <Route path="/More" element={<More />} />
+            <Route path="/Profile" element={<Profile />} />
+            <Route path="/Offers" element={<Offers />} />
+           
+ 
           </Routes>
-        {/* </AuthProvider> */}
       </BrowserRouter>
     </div>
   );
