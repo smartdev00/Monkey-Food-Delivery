74cbbe4877e7e337316e3ac35cac305fb67b5e02
smartdev00 <gladwynanderson95@gmail.com>
2024-06-25 17:23:18 -0700

    feat: add navbar all

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Basket from "./components/Icons/Basket";
 import ButtonShow from "./pages/ButtonShow";
+import StatusBar from "./components/Navbar/StatusBar";
+import Navbar from "./components/Navbar/Navbar";
+import Profile from "./pages/Profile";
 
 const App = () => {
   return (
     <div className="bg-slate-900 h-full min-h-screen">
+      <StatusBar/>
       <BrowserRouter>
         {/* <AuthProvider> */}
           <Routes>
             <Route path="/icon" element={<Basket />} />
             <Route path="/button" element={<ButtonShow />} />
+            <Route path="/profile" element={<Profile />} />
             {/* <Route path="/auth" element={<Authentication />} />
             <Route path="/" element={<Home />} />
             <Route path="/create-post" element={<CreatePost />} />
--- src/components/TextField/TextField.jsx
+++ src/components/TextField/TextField.jsx
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types'
 
 const TextField = (props) => {
     const { name, type, placeholder, value, onChange } = props;
+    const [ focus, setFoucs] = useState(false)
+
+    let classContent = "absolute left-[34px] text-[10px] top-5 "  + focus ? "text-main" : "text-secondary"
+
     return (
-        <div className=''>
+        <div className='relative'>
             <input
                 name = { name }
                 type={ type }
-                className='w-[315px] bg-[#f2f2f2] rounded-full text-[14px] pl-[34px] py-[18px] h-[56px] text-[#b7b6b6]'
-                placeholder={ placeholder }
+                className='w-[100%] bg-[#f2f2f2] rounded-full text-sm pl-[34px] py-[18px] h-[56px] text-primary'
+                onFocus={() => setFoucs(false)}
                 value = { value }
                 onChange={(e) => onChange(e.target.value)}
             />
+            <label className={classContent}>{ placeholder }</label>
         </div>
     );
 }
