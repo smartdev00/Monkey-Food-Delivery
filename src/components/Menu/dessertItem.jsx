a8335b7b97076bbdb46ecffc35f3338b4fe4cc25
smartdev00 <gladwynanderson95@gmail.com>
2024-06-27 11:38:24 -0700

    1,2 etc responsive modify to skh

--- src/components/Menu/dessertItem.jsx
+++ src/components/Menu/dessertItem.jsx
@@ -5,20 +5,18 @@ const DessertItem = (props) => {
     const { title, image, subtitle, rate } = props;
 
     return (
-        <div className="pt-[2px] ">
-            <div className="flex flex-col bg-no-repeat bg-contain "
-                style={{  backgroundImage: `url(${image})`  }}
+        <div className="relative pt-[2px] ">
+            <div className="flex flex-col bg-no-repeat bg-cover  "
+                style={{ backgroundImage: `url(${image})` }}
             >
-                {/* <img src={image} alt="ddd" /> */}
-                <div className="flex   pt-[126px] px-[21px]">
-                    <p className="text-white font-bold  text-[18px] ">{title}</p>
+                <div className="flex pt-[126px] px-[21px]">
+                    <p className="text-white font-bold text-[18px] ">{title}</p>
                 </div>
                 <div className="flex pt-[6px] px-[21px] gap-2">
                     <Icons type='star-filled' color='orange' />
-                    <p className="text-[#FC6011] font-bold  text-[12px] ">{rate}</p>
-                    <p className="text-white text-[12px] "> {subtitle}   </p>
+                    <p className="text-[#FC6011] font-bold text-[12px] ">{rate}</p>
+                    <p className="text-white text-[12px] "> {subtitle} </p>
                     <p className="text-white font-bold text-[14px] "> Desserts  </p>
-
                 </div>
             </div>
         </div>
--- src/pages/Menu/Dessert.jsx
+++ src/pages/Menu/Dessert.jsx
@@ -15,20 +15,20 @@ const Desserts = () => {
 
   return (
     <div className="relative">
-      <div className="px-[22px] ">
+      <div className="px-[22px]">
         <Navbar text="Desserts" shop="true" />
       </div>
       <div className="flex  flex-col  pt-[24px] pb-[19px] px-[21px]">
         <SearchBar placeholder=" Search Food" />
       </div>
-      <div className="flex flex-col   ">
+      <div className="flex flex-col ">
         <DessertItem image={ApplePie} title={'French Apple Pie'} rate={4.9} subtitle={'Minute by tuk tuk'} />
         <DessertItem image={Chocolate} title={'Dark Chocolate Cake'} rate={4.7} subtitle={'Cakes by Tella'} />
         <DessertItem image={Shake} title={'Street Shake'} rate={4.9} subtitle={'Café Race'} />
         <DessertItem image={Chewy} title={'Fudgy Chewy Brownies'} rate={4.9} subtitle={'Minute by tuk tuk'} />
       </div>
 
-      <div className="flex   pb-[138px]  ">  </div>
+      <div className="flex pb-[138px]">  </div>
       <Footer />
     </div>
   );
