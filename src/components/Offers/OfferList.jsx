5c48e66b9b4c6e640446a607cc8527f098bbbc51
smartdev00 <gladwynanderson95@gmail.com>
2025-05-16 03:25:49 +0800

    fix: write the correct name of OfferItem - OffersItem

--- src/components/Offers/OfferList.jsx
+++ src/components/Offers/OfferList.jsx
@@ -1,4 +1,4 @@
-import OfferItem from './OffersItem.jsx'
+import OffersItem from './OffersItem'
 
 import CafeImage from '../../../public/assets/img/offers/cafe.png'
 import IssoImage from '../../../public/assets/img/offers/isso.png'
@@ -8,9 +8,9 @@ const OfferList = () => {
 
   return (
     <>
-      <OfferItem image={CafeImage} title={'Café de Noires'} rate={4.9} subtitle={'(124 ratings) Café'} />
-      <OfferItem image={IssoImage} title={'Isso'} rate={4.9} subtitle={'(124 ratings) Café'} />
-      <OfferItem image={BeanImage} title={'Cafe Beans'} rate={4.9} subtitle={'(124 ratings) Café'} />
+      <OffersItem image={CafeImage} title={'Café de Noires'} rate={4.9} subtitle={'(124 ratings) Café'} />
+      <OffersItem image={IssoImage} title={'Isso'} rate={4.9} subtitle={'(124 ratings) Café'} />
+      <OffersItem image={BeanImage} title={'Cafe Beans'} rate={4.9} subtitle={'(124 ratings) Café'} />
     </>
   );
 };
